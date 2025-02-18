05ad7e6cb98bef007a6841a0775087d0c4ce0424
Yi Yao <leaf_yao8621@hotmail.com>
2025-02-17 18:16:49 -0800

    Cleanup

--- python/package/questa/simulator/src/simulator/simulator_run/simulator_run_max_weight.cpp
+++ python/package/questa/simulator/src/simulator/simulator_run/simulator_run_max_weight.cpp
@@ -10,43 +10,34 @@ void QUESTA::Simulator::run_max_weight(
     int cur_total_queue_length = 0;
     for (size_t i = 0; i < warmup; ++i) {
         mwm.run();
-        std::cout << "Service:" << std::endl;
         for (int j = 0; j <= this->lg.maxEdgeId(); ++j) {
             int flag =
                 em[this->lg.edgeFromId(j)] > 0 &&
                 mwm.matching(this->lg.edgeFromId(j));
             em[this->lg.edgeFromId(j)] -= flag;
-            std::cout << em[this->lg.edgeFromId(j)] << ' ';
             cur_total_queue_length -= flag;
         }
-        std::cout << std::endl << "Arrival:" << std::endl;
         for (int j = 0; j <= this->lg.maxEdgeId(); ++j) {
             int flag = this->gen() < (s / this->n);
             em[this->lg.edgeFromId(j)] += flag;
-            std::cout << em[this->lg.edgeFromId(j)] << ' ';
             cur_total_queue_length += flag;
         }
-        std::cout << std::endl;
     }
     int *ii = total_queue_length;
     for (size_t i = 0; i < n_iter; ++i, ++ii) {
         mwm.run();
-        std::cout << "Service:" << std::endl;
         for (int j = 0; j <= this->lg.maxEdgeId(); ++j) {
             int flag =
                 em[this->lg.edgeFromId(j)] > 0 &&
                 mwm.matching(this->lg.edgeFromId(j));
             em[this->lg.edgeFromId(j)] -= flag;
-            std::cout << em[this->lg.edgeFromId(j)] << ' ';
             cur_total_queue_length -= flag;
         }
         for (int j = 0; j <= this->lg.maxEdgeId(); ++j) {
             int flag = this->gen() < (s / this->n);
             em[this->lg.edgeFromId(j)] += flag;
-            std::cout << em[this->lg.edgeFromId(j)] << ' ';
             cur_total_queue_length += flag;
         }
-        std::cout << std::endl;
         *ii = cur_total_queue_length;
     }
 }
